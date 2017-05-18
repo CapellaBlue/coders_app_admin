b3918e9e9b646767f091557994459648806351bb
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-18 16:08:04 -0400

    Create new post and update a post work

--- public/app.js
+++ public/app.js
@@ -5,11 +5,16 @@ var app = angular.module('boardAdmin', []);
 app.controller('mainController', ['$http', function($http){
   //this.message = "angular works!";
   this.loggedIn = false;
-
+  this.posts = [];
+  this.postFormData = {};
+  this.postInd = -1;
+  this.viewOnePost = false;
+  this.editPostMode = false;
 
   // GET All Posts
   this.getAllPosts = function(){
     this.loggedIn = true;
+    this.viewOnePost = false;
     $http({
         method: 'GET',
         headers: {
@@ -22,4 +27,78 @@ app.controller('mainController', ['$http', function($http){
     }.bind(this));
   }
 
+  // See one Post and its Comments
+  this.showPostComments = function(post_id, ind){
+    this.viewPost = this.posts[ind];
+    $http({
+        method: 'GET',
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        url: 'http://localhost:3000/posts/'+post_id
+    }).then(function(response){
+      console.log(response.data.comments);
+      this.postComments = response.data.comments;
+      this.viewOnePost = true;
+    }.bind(this));
+  };
+
+  // Create A Post
+  this.createPost = function(){
+    console.log("inside createPost: ", this.postFormData);
+    $http({
+      method: 'POST',
+      url: 'http://localhost:3000/posts',
+      data: this.postFormData
+    }).then(function(result){
+      console.log('Data from server: ', result.data);
+      this.postFormData = {};
+      this.posts.unshift(result.data);
+    }.bind(this));
+  };
+
+  // Edit Post mode
+  this.editPost = function(ind){
+    this.editPostMode = true;
+    this.postFormData.title = this.posts[ind].title;
+    this.postFormData.content = this.posts[ind].content;
+    this.postFormData.author = this.posts[ind].author;
+    this.postInd = ind;
+  }
+
+  // Cancel Edit Post
+  this.cancelEditPost = function(){
+    this.editPostMode = false;
+    this.postFormData = {};
+  }
+
+  // Update Post in DB
+  this.updatePost = function(ind){
+    var tempId = this.posts[ind].id;
+    console.log("inside Update Post: ", this.postFormData);
+    $http({
+      method: 'PUT',
+      url: 'http://localhost:3000/posts/'+tempId,
+      data: this.postFormData
+    }).then(function(result){
+      console.log('Post updated from server: ', result.data);
+      this.editPostMode = false;
+      this.postFormData = {};
+      this.posts.splice(ind, 1, result.data);
+    }.bind(this));
+  }
+
+  // Back to view all posts
+  this.viewAllPosts = function(){
+    this.postFormData = {};
+    this.postInd = -1;
+    this.viewOnePost = false;
+    this.editPostMode = false;
+  }
+
+  // Fake Log off
+  this.logOff = function(){
+    this.loggedIn = false;
+    this.posts = [];
+  }
 }]);
