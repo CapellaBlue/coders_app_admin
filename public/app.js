350a928d4e67503186e163506f191fc9e5693c29
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-18 12:39:46 -0400

    Created a fake login page

--- public/app.js
+++ public/app.js
@@ -3,7 +3,23 @@ console.log("Admin app app.js");
 var app = angular.module('boardAdmin', []);
 
 app.controller('mainController', ['$http', function($http){
-  this.message = "angular works!";
+  //this.message = "angular works!";
+  this.loggedIn = false;
 
+
+  // GET All Posts
+  this.getAllPosts = function(){
+    this.loggedIn = true;
+    $http({
+        method: 'GET',
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        url: 'http://localhost:3000/posts'
+    }).then(function(response){
+        console.log(response.data);
+        this.posts = response.data;
+    }.bind(this));
+  }
 
 }]);
